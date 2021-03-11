af08a13184971a1455c229c6e7651d368f6f7de1
Jody <muhammad.adnand.jody@gmail.com>
2021-03-11 23:53:11 +0700

    Call API testing directly on Frontend

--- freetos/src/components/PhotoCard.js
+++ freetos/src/components/PhotoCard.js
@@ -6,32 +6,31 @@ function PhotoCard() {
   const [name, setName] = useState([]);
   useEffect(() => {
         axios.get(
-        "http://localhost:5000/api"
+        "http://api.flickr.com/services/feeds/photos_public.gne?tags=all&format=json&nojsoncallback=true"
         )
         .then(response => {
-            console.log("INI RESPONSE",response.data);
-            setName(response.data);
+            console.log("INI RESPONSE",response.data.items);
+            setName(response.data.items);
         })
   }, [])
   return (
-    <div>
+    <div className="container">
       {
         name.map((names) => {
             return(
-            <div>
-                <h1>{names.first_name}</h1>
+            <div className="">
+                <div className="card">
+                  <img src={names.media.m} className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">{names.title}</h5>
+                    <p className="card-text">by {names.author.split(" ")[1]}</p>
+                    <span class="badge bg-dark">Tag</span>
+                  </div>
+                </div>
             </div>
             )
         })
       }
-      <div className="card" style={{width: 18 + "rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
     </div>
   )
 }
