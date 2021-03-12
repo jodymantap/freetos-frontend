9531be631d47165aadee19707ef5e94ce8f15add
Jody <muhammad.adnand.jody@gmail.com>
2021-03-12 11:52:59 +0700

    Successfully Call API from Backend

--- freetos/src/components/PhotoCard.js
+++ freetos/src/components/PhotoCard.js
@@ -6,31 +6,35 @@ function PhotoCard() {
   const [name, setName] = useState([]);
   useEffect(() => {
         axios.get(
-        "http://api.flickr.com/services/feeds/photos_public.gne?tags=all&format=json&nojsoncallback=true"
+        "http://localhost:5000/api"
         )
         .then(response => {
-            console.log("INI RESPONSE",response.data.items);
-            setName(response.data.items);
+            console.log("INI RESPONSE",response.data[0]);
+            setName(response.data[0]);
         })
   }, [])
   return (
-    <div className="container">
+    <div className="container row-cols-2">
+      <div className="">
       {
         name.map((names) => {
             return(
-            <div className="">
-                <div className="card">
-                  <img src={names.media.m} className="card-img-top" alt="..."/>
-                  <div className="card-body">
-                    <h5 className="card-title">{names.title}</h5>
-                    <p className="card-text">by {names.author.split(" ")[1]}</p>
-                    <span class="badge bg-dark">Tag</span>
+            <div className="col">
+              <div className="">
+                  <div className="card">
+                    <img src={names.media.m} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                      <h5 className="card-title">{names.title}</h5>
+                      <p className="card-text">by {names.author.split(" ")[1]}</p>
+                      <span class="badge bg-dark">Tag</span>
+                    </div>
                   </div>
-                </div>
+              </div>
             </div>
             )
         })
       }
+      </div>
     </div>
   )
 }
